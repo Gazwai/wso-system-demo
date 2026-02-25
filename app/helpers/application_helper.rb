9e39e66c1f9f5005fd3f05f8c0658eb1d2dbeb77
Gary Y <84076465+Gazwai@users.noreply.github.com>
2026-02-25 21:11:00 +0900

    style: Add style to sidebar and add helper function to get menu items hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def menu_items
+    [
+      { icon: "fa-regular fa-house", text: "Dashboard", active: true },
+      { icon: "fa-regular fa-calendar", text: "Calendar", active: false },
+      { icon: "fa-solid fa-trophy", text: "Tournaments", active: false },
+      { icon: "fa-regular fa-message", text: "Messages", active: false },
+      { icon: "fa-regular fa-folder", text: "Resources", active: false },
+      { icon: "fa-regular fa-circle-question", text: "Help", active: false },
+    ]
+  end
 end
